5942998f2057b2a576eaa88f26c0af05865781a8
HAP <hproctor@redhat.com>
2022-11-17 06:05:21 -0500

    * Cleans up code * Adds license header * Adds requirements.txt

--- src/sa_metameta/exceptions.py
+++ src/sa_metameta/exceptions.py
@@ -1,9 +1,24 @@
+#    Copyright 2022 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 
 class MetaMetaException(Exception):
+    """Base MetaMeta exception."""
     pass
 
 class MetaMetaNotFound(MetaMetaException):
+    """Base MetaMeta item, attribute not found exception."""
     _item_type = "item"
     def __init__(self, *args):
         if len(args) < 1:
@@ -26,10 +41,22 @@ class MetaMetaNotFound(MetaMetaException):
         super().__init__(*new_args)
 
 class MetaMetaEngineNotFound(MetaMetaNotFound):
+    """Base MetaMetaEngine item, attribute not found exception."""
     _item_type = "engine"
 
 class MetaMetaSchemaNotFound(MetaMetaNotFound):
+    """Base MetaMetaSchema item, attribute not found exception."""
     _item_type = "schema"
 
 class MetaMetaTableNotFound(MetaMetaNotFound):
+    """Base MetaMetaTable item, attribute not found exception."""
     _item_type = "table"
+
+
+__all__ = (
+    "MetaMetaException",
+    "MetaMetaNotFound",
+    "MetaMetaEngineNotFound",
+    "MetaMetaSchemaNotFound",
+    "MetaMetaTableNotFound",
+)
